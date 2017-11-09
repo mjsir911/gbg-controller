97bf0a648be11f43731b61b1952a94f25802abac
Marco Sirabella <marco@sirabella.org>
2017-11-09 12:14:24 -0500

    Update constants to be handled better

--- gbg_talk.js
+++ gbg_talk.js
@@ -1,12 +1,15 @@
+const var LED_SERVICE = 0xC2BD
+const var LED_ON_CHAR = 0xC24C
+
 var LED_characteristic;
 function bt_init() {
 	LED_characteristic = navigator.bluetooth.requestDevice({
 	  acceptAllDevices:true,
-	  optionalServices: [0x00FF],
+	  optionalServices: [LED_SERVICE],
 	})
 	.then(device => device.gatt.connect())
-	.then(server => server.getPrimaryService(0x00FF))
-	.then(service => service.getCharacteristic(0xFF01));
+	.then(server => server.getPrimaryService(LED_SERVICE))
+	.then(service => service.getCharacteristic(LED_ON_CHAR));
 	LED_characteristic.then(characteristic => characteristic.writeValue(Uint8Array.of(0)));
 }
 
